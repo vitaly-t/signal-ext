9af578780bf5cea06320afc48235edb55a157d8d
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2023-06-04 12:26:09 +0100

    improve code

--- signal-ext.ts
+++ signal-ext.ts
@@ -1,25 +1,35 @@
 import {signal as _signal, CreateSignalOptions, WritableSignal} from '@angular/core';
 
+/**
+ * Additional options when creating a signal.
+ */
 export interface ICreateSignalOptionsExt<T> extends CreateSignalOptions<T> {
     name?: string;
 }
 
+/**
+ * Extension properties that are set internally.
+ */
 interface ICreateSignalExt<T> extends ICreateSignalOptionsExt<T> {
     readonly created: Date;
 }
 
+/**
+ * Extended signal signature, with `ext` namespace.
+ */
 export interface IWritableSignalExt<T> extends WritableSignal<T> {
     readonly ext: ICreateSignalExt<T>;
 }
 
+/**
+ * Signal creation override, to support `ext` namespace + additional initialization options.
+ */
 export function signal<T>(initialValue: T, options?: ICreateSignalOptionsExt<T>): IWritableSignalExt<T> {
     const s = _signal(initialValue, options);
-    Object.defineProperty(s, 'ext', {
-        value: {
-            created: new Date(),
-            name: options?.name
-        },
-        writable: false
-    });
-    return s as IWritableSignalExt<T>;
+    const value: ICreateSignalExt<T> = {
+        created: new Date(),
+        name: options?.name
+    }
+    Object.defineProperty(s, 'ext', {value, writable: false});
+    return s;
 }
